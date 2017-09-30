4c6bdcec157667947cbb8ca434e69f8e917aacb9
chris48s <chris.shaw480@gmail.com>
2017-09-30 15:11:10 +0100

    work out dataset ids instead of hard-coding them

--- scraper.py
+++ scraper.py
@@ -1,10 +1,25 @@
+import json
+from dc_base_scrapers.common import get_data_from_url
 from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
 
-stations_url = "http://opendata-tunbridgewells.opendata.arcgis.com/datasets/feb0be539f0c4c3b97bcaf812f930282_6.geojson"
-districts_url = "http://opendata-tunbridgewells.opendata.arcgis.com/datasets/3eeae54e88894e6ba65f6451c8e417f1_5.geojson"
+def get_dataset_id(search_url):
+    search_str = get_data_from_url(search_url)
+    search_data = json.loads(search_str.decode('utf-8'))
+    if len(search_data['data']) != 1:
+        raise ValueError("Failed to find a dataset id")
+    else:
+        return search_data['data'][0]['id']
+
+
+stations_search_url = "http://opendata-tunbridgewells.opendata.arcgis.com/datasets.json?q=polling%20station&sort_by=relevance"
+districts_search_url = "http://opendata-tunbridgewells.opendata.arcgis.com/datasets.json?q=polling%20district&sort_by=relevance"
+stations_base_url = "http://opendata-tunbridgewells.opendata.arcgis.com/datasets/%s.geojson"
+districts_base_url = "http://opendata-tunbridgewells.opendata.arcgis.com/datasets/%s.geojson"
 council_id = 'E07000116'
 
+stations_url = stations_base_url % (get_dataset_id(stations_search_url))
+districts_url = districts_base_url % (get_dataset_id(districts_search_url))
 
 stations_scraper = GeoJsonScraper(stations_url, council_id, 'utf-8', 'stations', key='OBJECTID')
 stations_scraper.scrape()
