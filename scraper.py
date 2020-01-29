ec8b05d625181a5aeddf61c52ae92aadbd218525
chris48s <chris.shaw480@gmail.com>
2020-01-29 15:42:15 +0000

    update URLs

--- scraper.py
+++ scraper.py
@@ -2,24 +2,9 @@ import json
 from dc_base_scrapers.common import get_data_from_url
 from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
-
-def get_dataset_id(search_url):
-    search_str = get_data_from_url(search_url)
-    search_data = json.loads(search_str.decode('utf-8'))
-    if len(search_data['data']) != 1:
-        raise ValueError("Failed to find a dataset id")
-    else:
-        return search_data['data'][0]['id']
-
-
-stations_search_url = "https://opendata.arcgis.com/api/v2/datasets?filter%5Bcatalogs%5D=opendatanew-tunbridgewells.opendata.arcgis.com&include=organizations%2Cgroups&page%5Bnumber%5D=1&page%5Bsize%5D=10&q=polling+AND+stations"
-districts_search_url = "https://opendata.arcgis.com/api/v2/datasets?filter%5Bcatalogs%5D=opendatanew-tunbridgewells.opendata.arcgis.com&include=organizations%2Cgroups&page%5Bnumber%5D=1&page%5Bsize%5D=10&q=polling+districts"
-stations_base_url = "http://opendatanew-tunbridgewells.opendata.arcgis.com/datasets/%s.geojson"
-districts_base_url = "http://opendatanew-tunbridgewells.opendata.arcgis.com/datasets/%s.geojson"
+stations_url = "https://opendata.arcgis.com/datasets/ea6894c98dfb4cf99775d1fb949cf0de_6.geojson"
+districts_url = "https://opendata.arcgis.com/datasets/24b6d662cd1145eba4743d27e91c0436_5.geojson"
 council_id = 'E07000116'
-
-stations_url = stations_base_url % (get_dataset_id(stations_search_url))
-districts_url = districts_base_url % (get_dataset_id(districts_search_url))
 
 print(stations_url)
 stations_scraper = GeoJsonScraper(stations_url, council_id, 'utf-8', 'stations', key='OBJECTID')
