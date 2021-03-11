42329a700fb6521dddac8a477a4e6c2c8201251b
GeoWill <roper.william@gmail.com>
2021-03-11 14:35:46 +0000

    Update council id

--- scraper.py
+++ scraper.py
@@ -1,10 +1,8 @@
-import json
-from dc_base_scrapers.common import get_data_from_url
 from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
 stations_url = "https://opendata.arcgis.com/datasets/ea6894c98dfb4cf99775d1fb949cf0de_6.geojson"
 districts_url = "https://opendata.arcgis.com/datasets/24b6d662cd1145eba4743d27e91c0436_5.geojson"
-council_id = 'E07000116'
+council_id = 'TUN'
 
 print(stations_url)
 stations_scraper = GeoJsonScraper(stations_url, council_id, 'utf-8', 'stations', key='OBJECTID')
